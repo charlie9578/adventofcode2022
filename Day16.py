0ce0e0475ea30e19bff458e5c02c7fa7bce5cc30
charlie9578 <charlie.plumley@gmail.com>
2023-04-02 17:04:25 +0100

    Day16 part 2 complete :)

--- Day16.py
+++ Day16.py
@@ -150,8 +150,11 @@ Work out the steps to release the most pressure in 30 minutes. What is the most 
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
+import time
 
-file_name = r"day/16/input_example"
+timer = time.time()
+
+file_name = r"day/16/input"
 valve=[]
 flow=[]
 target=[]
@@ -174,13 +177,204 @@ df = pd.DataFrame(list(zip(valve,target,flow)),
 
 print(df)
 
-G = nx.from_pandas_edgelist(df, source='Source', target='Target')
+G = nx.from_pandas_edgelist(df, source="Source", target="Target")
 
 nx.draw(G, with_labels=True, font_weight='bold')
 
+# plt.show()
+
 df_useful = df[["Source","Flow"]].drop_duplicates()
 df_useful = df_useful[df_useful["Flow"]>0]
+df_useful = df_useful.set_index("Source")
 
 print(df_useful)
 
-plt.show()
+shortest_paths = dict(nx.shortest_path_length(G))
+# print(shortest_paths)
+
+flows = dict(zip(valve,flow))
+print(flows)
+
+
+def check_routes(path,time_remaining,discharge,max_discharge):
+    current_node = path[-1]
+    potential_nodes = set(df_useful.index)
+
+    for node in potential_nodes-set(path):
+        new_path = path.copy()
+        distance = shortest_paths[current_node][node]
+        new_time_remaining = time_remaining - distance - 1
+
+        if new_time_remaining>=0:
+            new_path.append(node)
+            flow = flows[node]
+            new_discharge = discharge + flow*new_time_remaining
+
+            if new_discharge>max_discharge:
+                max_discharge=new_discharge
+                print(f"Route {path} produced a discharge of {new_discharge} with {new_time_remaining} time remaining")
+
+            max_discharge=check_routes(new_path,new_time_remaining,discharge=new_discharge,max_discharge=max_discharge)
+
+    return max_discharge
+
+
+time_remaining = 30
+discharge = 0
+max_discharge = 0
+path = ["AA"]
+max_discharge=check_routes(path=path,time_remaining=time_remaining,discharge=discharge,max_discharge=max_discharge)
+print(max_discharge)
+
+print(f"Time elapsed: {time.time()-timer}s")
+
+
+"""
+Your puzzle answer was 1673.
+
+The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+You're worried that even with an optimal approach, the pressure released won't be enough. What if you got one of the elephants to help you?
+
+It would take you 4 minutes to teach an elephant how to open the right valves in the right order, leaving you with only 26 minutes to actually execute your plan. Would having two of you working together be better, even if it means having less time? (Assume that you teach the elephant before opening any valves yourself, giving you both the same full 26 minutes.)
+
+In the example above, you could teach the elephant to help you as follows:
+
+== Minute 1 ==
+No valves are open.
+You move to valve II.
+The elephant moves to valve DD.
+
+== Minute 2 ==
+No valves are open.
+You move to valve JJ.
+The elephant opens valve DD.
+
+== Minute 3 ==
+Valve DD is open, releasing 20 pressure.
+You open valve JJ.
+The elephant moves to valve EE.
+
+== Minute 4 ==
+Valves DD and JJ are open, releasing 41 pressure.
+You move to valve II.
+The elephant moves to valve FF.
+
+== Minute 5 ==
+Valves DD and JJ are open, releasing 41 pressure.
+You move to valve AA.
+The elephant moves to valve GG.
+
+== Minute 6 ==
+Valves DD and JJ are open, releasing 41 pressure.
+You move to valve BB.
+The elephant moves to valve HH.
+
+== Minute 7 ==
+Valves DD and JJ are open, releasing 41 pressure.
+You open valve BB.
+The elephant opens valve HH.
+
+== Minute 8 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You move to valve CC.
+The elephant moves to valve GG.
+
+== Minute 9 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You open valve CC.
+The elephant moves to valve FF.
+
+== Minute 10 ==
+Valves BB, CC, DD, HH, and JJ are open, releasing 78 pressure.
+The elephant moves to valve EE.
+
+== Minute 11 ==
+Valves BB, CC, DD, HH, and JJ are open, releasing 78 pressure.
+The elephant opens valve EE.
+
+(At this point, all valves are open.)
+
+== Minute 12 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+...
+
+== Minute 20 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+...
+
+== Minute 26 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+With the elephant helping, after 26 minutes, the best you could do would release a total of 1707 pressure.
+
+With you and an elephant working together for 26 minutes, what is the most pressure you could release?
+"""
+
+max_discharge_possible = check_routes(path=["AA"],time_remaining=26,discharge=0,max_discharge=0)
+
+def check_routes1(path,time_remaining,discharge,max_discharge):
+    current_node = path[-1]
+    potential_nodes = set(df_useful.index)
+
+    for node in potential_nodes-set(path):
+        new_path = path.copy()
+        distance = shortest_paths[current_node][node]
+        new_time_remaining = time_remaining - distance - 1
+
+        if new_time_remaining>=0:
+            new_path.append(node)
+            flow = flows[node]
+            new_discharge = discharge + flow*new_time_remaining
+
+            if new_discharge + max_discharge_possible >= max_discharge:
+                second_discharge,best_path,best_remaining_time = check_routes2(path1=new_path,path=["AA"],time_remaining=26,discharge=0,max_discharge=0,best_path=[],best_remaining_time=0)
+            else:
+                best_path=[]
+                best_remaining_time = 0
+                second_discharge = 0
+
+            if new_discharge + second_discharge > max_discharge:
+                max_discharge = new_discharge + second_discharge
+                print(f"Max discharge {max_discharge}")
+                print(f"Route1 {new_path} produced a discharge of {new_discharge} with {new_time_remaining} time remaining")
+                print(f"Route2 {best_path} produced a discharge of {second_discharge} with {best_remaining_time} time remaining")
+
+            max_discharge=check_routes1(new_path,new_time_remaining,discharge=new_discharge,max_discharge=max_discharge)
+
+    return max_discharge
+
+
+def check_routes2(path1,path,time_remaining,discharge,max_discharge,best_path,best_remaining_time):
+    current_node = path[-1]
+    potential_nodes = set(df_useful.index)-set(path1)
+
+
+    for node in potential_nodes-set(path):
+        new_path = path.copy()
+        distance = shortest_paths[current_node][node]
+        new_time_remaining = time_remaining - distance - 1
+
+        if new_time_remaining>=0:
+            new_path.append(node)
+            flow = flows[node]
+            new_discharge = discharge + flow*new_time_remaining
+
+            if new_discharge>max_discharge:
+                max_discharge=new_discharge
+                best_path = new_path
+                best_remaining_time = new_time_remaining
+                # print(f"Route2 {path} produced a discharge of {new_discharge} with {new_time_remaining} time remaining")
+
+            max_discharge,best_path,best_remaining_time=check_routes2(path1,new_path,new_time_remaining,discharge=new_discharge,max_discharge=max_discharge,best_path=best_path,best_remaining_time=best_remaining_time)
+
+    return max_discharge,best_path,best_remaining_time
+
+max_discharge=check_routes1(path=["AA"],time_remaining=26,discharge=0,max_discharge=0)
+print(max_discharge)
+
+print(f"Time elapsed: {time.time()-timer}s")
+
